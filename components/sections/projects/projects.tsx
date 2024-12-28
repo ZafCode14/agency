e1eca57ecfdd42d7aa4e7d6012f3ec77fd598070
ZafCode14 <zafarwp14@gmail.com>
2024-12-28 07:21:04 +0200

    safari blur

--- components/sections/projects/projects.tsx
+++ components/sections/projects/projects.tsx
@@ -4,7 +4,10 @@ const ProjectsSection = () => {
       sticky top-[70px]
       w-[1200px] max-w-full
     `}>
-        <div className={`w-full flex flex-col items-center bg-[#2a6077ab] backdrop-blur-md rounded-t-[30px] h-screen absolute`}>
+        <div className={`w-full flex flex-col items-center bg-[#2a6077ab] backdrop-blur-md rounded-t-[30px] h-screen absolute`} style={{
+          WebkitBackdropFilter: 'blur(20px)', 
+          backdropFilter: 'blur(20px)'
+        }}>
           <h2 className="font-bold text-[24px] md:text-[34px] my-10 text-[white]">Discover our latest projects</h2>
         </div>
     </section>
