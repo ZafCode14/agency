de24431f45894e20290fe7e37d321cb234a2269e
ZafCode14 <zafarwp14@gmail.com>
2025-02-01 11:35:59 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const mulish = Mulish({
 
 export const metadata: Metadata = {
   title: "Etavelle",
-  description: "Create Amazing Websites",
+  description: "Etavelle offers expert web development services, specializing in high-performance, SEO-optimized websites tailored to elevate your business presence online. From stunning designs to powerful functionalities, we create impactful digital experiences.",
 };
 
 export default function RootLayout({
@@ -23,7 +23,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo2.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+
+        <meta property="og:title" content="Etavelle" />
+        <meta property="og:description" content="Etavelle offers expert web development services, specializing in high-performance, SEO-optimized websites tailored to elevate your business presence online. From stunning designs to powerful functionalities, we create impactful digital experiences." />
+        <meta property="og:image" content="https://i.ibb.co/Hmp69hV/logo.png"/>
+        <meta property="og:url" content="https://etavelle.com" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
       </head>
       <body className={`${mulish.className}`} suppressHydrationWarning={true}>
         <Header />
